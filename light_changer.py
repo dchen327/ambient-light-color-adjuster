48bf5ac8cdaceff6b329852cac5d8dc1664c09d3
David Chen <davidc3287@gmail.com>
2020-05-18 14:10:39 -0400

    initial design, small screenshot region, constant color checks and updates

--- light_changer.py
+++ light_changer.py
@@ -1,13 +1,43 @@
 import requests
+import pyscreenshot as ImageGrab
+from colorthief import ColorThief
 
-token = ""
 
-headers = {
-    "Authorization": "Bearer %s" % token,
-}
+def get_dominant_colour():
+    """ Screenshot a portion of the screen and return the rgb tuple of the most dominant color """
+    im = ImageGrab.grab(bbox=(0, 1060, 10, 1080), backend='mss', childprocess=False)
+    im.save('screenshot.png')
+    color_thief = ColorThief('screenshot.png')
+    dominant_color = color_thief.get_color(quality=1)
+    
+    return dominant_color
 
-payload = {
-    "color": "#ffffff",
-}
 
-response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+def set_light_color(color):
+    """ Set lifx light color to provided rgb tuple """
+    rgb = 'rgb:' + ','.join(map(str, color))  # convert (r, g, b) -> rgb:r,g,b
+
+    token = ""
+
+    headers = {
+        "Authorization": "Bearer %s" % token,
+    }
+
+    payload = {
+        "color": rgb,
+    }
+
+    response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
+    return response
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            color = get_dominant_colour()
+            set_light_color(color)
+        except KeyboardInterrupt:
+            set_light_color((255, 255, 255))
+            break
+
+
